da06ea78e5bab99261b652b8fbe73bdef09be26d
Oluwakemi96 <rashidatoluwakemi96@gmail.com>
2022-10-12 14:03:15 +0100

    created the application table

---
+++ migrations/sqls/20221012105618-application-table-up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE applications(
+  id SERIAL PRIMARY KEY NOT NULL,
+  upload_CV TEXT NOT NULL,
+  upload_photo TEXT NOT NULL,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
+  date_of_birth VARCHAR NOT NULL,
+  address VARCHAR NOT NULL,
+  university VARCHAR NOT NULL,
+  course_of_study VARCHAR NOT NULL,
+  cgpa INT NOT NULL
+);
