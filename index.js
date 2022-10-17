ead11983ed8b32d0121477877f561bc69fe2e0a5
Oluwakemi96 <rashidatoluwakemi96@gmail.com>
2022-10-17 09:58:40 +0100

    set Header

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT
 
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080/');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS, PATCH');
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type, Origin, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next();
+})
 
 app.use(router)
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+// const cors = require('cors');
 const user = require('../controllers/signup.controller')
 const application = require('../controllers/application.controller')
 const userLogin = require('../controllers/login.controller')
 
+// const app = express()
+// app.use(cors({
+//   origin: "http://localhost:8080",
+//   methods: ["GET", "POST"]
+// }))
 
 router.post('/signup', user.registerUsers);
 router.delete('/signup/:id', user.deleteUser);
