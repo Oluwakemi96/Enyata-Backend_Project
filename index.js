0d36835dc2a43613af7a294083cc9334228a041f
oluwadamilolaoyelade <107127029+oluwadamilolaoyelade@users.noreply.github.com>
2022-10-18 10:28:03 +0100

    init

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = require('./src/routes/user.route')
 const adminRouter = require('./src/routes/admin.route')
 const dotenv = require('dotenv')
+const cors = require('cors')
 dotenv.config()
 
 const app = express()
@@ -11,14 +12,17 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS, PATCH');
-  res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type, Origin, Accept');
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  next();
-})
-
+// app.use(function (req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS, PATCH');
+//   res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type, Origin, Accept');
+//   res.setHeader('Access-Control-Allow-Credentials', true)
+//   next();
+// })
+app.use(cors({
+  origin: "*" ,
+  methods: ["GET", "POST", "DELETE", "PATCH", "UPDATE", "OPTIONS", "PUT"]
+}))
 app.use(router)
 app.use(adminRouter)
 
