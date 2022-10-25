1a9d611b3833b91ea45d6879bc7c313c546a35c7
oluwadamilolaoyelade <107127029+oluwadamilolaoyelade@users.noreply.github.com>
2022-10-25 15:39:26 +0100

    ...

--- src/controllers/composed.controller.js
+++ src/controllers/composed.controller.js
@@ -1,16 +1,16 @@
 const db = require('../config/config')
 const queries = require('../queries/composed.queries')
-const applicationsQueries = require('../queries/application.queries')
+// const applicationsQueries = require('../queries/application.queries')
 
 const composedAssessments = async(req, res) => {
-    let {questions, time_allocated} = req.body
-    let batch = await db.any(applicationsQueries.getActiveBatch)
-    batch_id = batch[0].batch_id
+    let {questions, option_a, option_b, option_c, option_d } = req.body
+    // let batch = await db.any(applicationsQueries.getActiveBatch)
+    // batch_id = batch[0].batch_id
     try {
-        const assessments = await db.any(queries.postQuestion,[ batch_id, questions, time_allocated]);
+        const assessments = await db.any(queries.postQuestion,[ questions, option_a, option_b, option_c, option_d]);
             return res.status(200).json({
                 status: 'Success',
-                message: 'Batch added',
+                message: 'question added',
                 data: assessments
             })
         
@@ -20,6 +20,28 @@ const composedAssessments = async(req, res) => {
     }
 }
 
+const getQuestionById = async(req, res) => {
+    let { id } = req.params
+    try {
+        const question = await db.oneOrNone(`SELECT * 
+     FROM
+        assessments       
+     WHERE 
+        id = '${id}'`)
+        console.log(id)
+        console.log(question)
+        return res.status(200).json({
+            status: 'Success',
+            message: 'question fetched succesfully',
+            data: question
+        });
+    } catch (error) {
+        console.log(error)
+        return error;
+    }
+}
+
 module.exports = {
-    composedAssessments
+    composedAssessments,
+    getQuestionById
 }
--- src/queries/composed.queries.js
+++ src/queries/composed.queries.js
@@ -1,6 +1,8 @@
 const queries = {
     postQuestion:
-    `INSERT INTO assessments (batch_id, questions, time_allocated)
-    VALUES ($1, $2, $3) 
+    `INSERT INTO assessments (questions, option_a, option_b, option_c, option_d )
+    VALUES ($1, $2, $3, $4, $5) 
     RETURNING * `,
 }
+
+module.exports = queries
