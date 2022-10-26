486a6fd9e61bf46b8c502a32516ba77c361624c2
oluwadamilolaoyelade <107127029+oluwadamilolaoyelade@users.noreply.github.com>
2022-10-26 23:36:57 +0100

    ..

--- src/controllers/composed.controller.js
+++ src/controllers/composed.controller.js
@@ -4,10 +4,11 @@ const applicationsQueries = require('../queries/application.queries')
 
 const composedAssessments = async(req, res) => {
     let {questions, time_allocated} = req.body
+   const question = questions.stringfy
     let batch = await db.any(applicationsQueries.getActiveBatch)
     batch_id = batch[0].batch_id
     try {
-        const assessments = await db.any(queries.postQuestion,[ batch_id, questions, time_allocated]);
+        const assessments = await db.any(queries.postQuestion,[ batch_id, question, time_allocated]);
             return res.status(200).json({
                 status: 'Success',
                 message: 'Batch added',
