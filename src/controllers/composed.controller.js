7ccdfb037aaeb859573dd931388bad942b9d9185
oluwadamilolaoyelade <107127029+oluwadamilolaoyelade@users.noreply.github.com>
2022-10-27 01:25:15 +0100

    ...

--- src/controllers/composed.controller.js
+++ src/controllers/composed.controller.js
@@ -3,7 +3,10 @@ const queries = require('../queries/composed.queries')
 const applicationsQueries = require('../queries/application.queries')
 
 const composedAssessments = async(req, res) => {
-    let {questions, time_allocated} = JSON.stringify(req.body)
+    
+    let {time_allocated} = req.body
+    let questions = JSON.stringify(req.body.questions)
+
     let batch = await db.any(applicationsQueries.getActiveBatch)
     batch_id = batch[0].batch_id
     try {
